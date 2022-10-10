a75c2d5c3554df0384f09dedc41883032581815a
Sjoerd van den Broek <sjoerdv.d.broek@live.nl>
2022-10-10 18:29:43 +0200

    Debugging update

--- Parking Game/Assets/Resources/UI/UIAlignColourWithButton.cs
+++ Parking Game/Assets/Resources/UI/UIAlignColourWithButton.cs
@@ -7,15 +7,34 @@ using UnityEngine.UI;
 public class UIAlignColourWithButton : MonoBehaviour {
 
     public Color32 defaultColor;
+    public Color32 hoverColor;
+    public Color32 pressedColor;
+    public Color32 disabledColor;
     public Image[] images;
 
     void Start() {
         this.ApplyColour(this.defaultColor);
     }
 
-    public void ApplyColour(Color32 color) {
+    public void ApplyDefault() {
+        this.ApplyColour(this.defaultColor);
+    }
+
+    public void ApplyHover() {
+        this.ApplyColour(this.hoverColor);
+    }
+
+    public void ApplyPressed() {
+        this.ApplyColour(this.pressedColor);
+    }
+
+    public void ApplyDisabled() {
+        this.ApplyColour(this.disabledColor);
+    }
+
+    public void ApplyColour(Color color) {
         for (int i = 0; i < this.images.Length; i++) {
-            this.images[i].color = this.defaultColor;
+            this.images[i].color = color;
         }
     }
 }
